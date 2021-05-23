032e0f45304f7b850b17b7e8362100e2e18cb572
2019211001001002-MiJiang <3072578962@qq.com>
2021-05-24 07:04:36 +0800

    2.HW-CE2 Commit

---
+++ src/main/java/com/MiJiang/controller/ProductDetailsServlet.java
@@ -0,0 +1,40 @@
+package main.java.com.MiJiang.controller;
+
+import main.java.com.MiJiang.dao.ProductDao;
+import main.java.com.MiJiang.model.Category;
+import main.java.com.MiJiang.model.Product;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet(name = "ProductDetailsServlet",value= "/productDetails")
+public class ProductDetailsServlet extends HttpServlet {
+    Connection con=null;
+    public void init() throws ServletException{
+        super.init();
+        con=(Connection)getServletContext().getAttribute("con");
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id=request.getParameter("id")!=null?Integer.parseInt(request.getParameter("id")):0;
+        ProductDao productDao=new ProductDao();
+        if(id==0){
+            return;
+        }
+        List<Category> categoryList=Category.findAllCategory(con);
+        request.setAttribute("categoryList",categoryList);
+        Product product=productDao.findById(id,con);
+        request.setAttribute("p",product);
+        String path="/WEB-INF/views/productDetails.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+}
